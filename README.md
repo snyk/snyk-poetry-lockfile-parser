6dbbc2c1b014be701f38c2e51c6cee0b1f6b2821
Daniel Trunley <daniel.trunley@snyk.io>
2020-09-28 14:21:26 +0100

    feat: added a skeleton inspect method for invoking from Snyk CLI

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ***
 
-[![Known Vulnerabilities](https://snyk.io/test/github/snyk/nodejs-lockfile-parser/badge.svg)](https://snyk.io/test/github/snyk/nodejs-lockfile-parser)
+[![Known Vulnerabilities](https://snyk.io/test/github/snyk/snyk-poetry-lockfile-parser/badge.svg)](https://snyk.io/test/github/snyk/snyk-poetry-lockfile-parser/badge.svg)
 
 
 Snyk helps you find, fix and monitor for known vulnerabilities in your dependencies, both on an ad hoc basis and as part of your CI (Build) system.
--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,64 @@ import {
   DepGraphBuilder,
 } from '@snyk/dep-graph';
 import { PoetryManifestType } from './types/poetry-manifest-type';
+import { legacyPlugin as api } from '@snyk/cli-interface';
+import debugModule = require('debug');
+
+// To enable debugging output, use `snyk -d`
+let logger: debugModule.Debugger | null = null;
+function debugLog(s: string) {
+  if (logger === null) {
+    // Lazy init: Snyk CLI needs to process the CLI argument "-d" first.
+    // TODO(BST-648): more robust handling of the debug settings
+    if (process.env.DEBUG) {
+      debugModule.enable(process.env.DEBUG);
+    }
+    logger = debugModule('snyk-gradle-plugin');
+  }
+  logger(s);
+}
+
+type Options = api.InspectOptions;
+
+// Overload type definitions, so that when you call inspect() with an `options` literal (e.g. in tests),
+// you will get a result of a specific corresponding type.
+export async function inspect(
+  root: string,
+  targetFile: string,
+  options?: api.SingleSubprojectInspectOptions,
+): Promise<api.SinglePackageResult>;
+export async function inspect(
+  root: string,
+  targetFile: string,
+  options: api.MultiSubprojectInspectOptions,
+): Promise<api.MultiProjectResult>;
+
+// TODO: Specify result types vs any
+// General implementation. The result type depends on the runtime type of `options`.
+export async function inspect(
+  root: string,
+  targetFile: string,
+  options?: Options,
+): Promise<api.InspectResult> {
+  debugLog(
+    "Poetry inspect called with: " +
+    JSON.stringify({
+      root,
+      targetFile,
+      allSubProjects: (options as any)?.allSubProjects,
+      subProject: (options as any)?.subProject,
+    }),
+  );
+
+  const depGraph = new DepGraphBuilder({ name: 'poetry' }).build();
+
+  return {
+    plugin: { name: "plugin" },
+    package: { },
+    dependencyGraph: depGraph
+  };
+}
+
 
 export function buildDepGraph(
   manifestFileContents: string,
@@ -19,6 +77,7 @@ export function buildDepGraph(
 
   const manifestFile: PoetryManifestType = toml.parse(manifestFileContents);
   const lockFile: PoetryLockFile = toml.parse(lockFileContents);
+  console.log('manifestFile: ', manifestFile);
   console.log(lockFile, manifestFile, includeDev);
 
   const pkgManager: PkgManager = { name: 'name' };
