8cc11700b162a901cccd0a3d7f1102c5e755acc3
Darren McGuinness <darren.mcguinness@snyk.io>
2020-09-29 16:28:16 +0100

    Added support for generating graphs with transitive dependencies.

--- lib/index.ts
+++ lib/index.ts
@@ -1,75 +1,11 @@
 import * as toml from 'toml';
 import { PoetryLockFile } from './types/poetry-lock-file-type';
-import {
-  PkgInfo,
-  PkgManager,
-  DepGraph,
-  DepGraphBuilder,
-} from '@snyk/dep-graph';
+import { DepGraph, DepGraphBuilder, PkgManager } from '@snyk/dep-graph';
 import { PoetryManifestType } from './types/poetry-manifest-type';
-import { legacyPlugin as api } from '@snyk/cli-interface';
-import debugModule = require('debug');
-
-// To enable debugging output, use `snyk -d`
-let logger: debugModule.Debugger | null = null;
-function debugLog(s: string) {
-  if (logger === null) {
-    // Lazy init: Snyk CLI needs to process the CLI argument "-d" first.
-    // TODO(BST-648): more robust handling of the debug settings
-    if (process.env.DEBUG) {
-      debugModule.enable(process.env.DEBUG);
-    }
-    logger = debugModule('snyk-gradle-plugin');
-  }
-  logger(s);
-}
-
-type Options = api.InspectOptions;
-
-// Overload type definitions, so that when you call inspect() with an `options` literal (e.g. in tests),
-// you will get a result of a specific corresponding type.
-export async function inspect(
-  root: string,
-  targetFile: string,
-  options?: api.SingleSubprojectInspectOptions,
-): Promise<api.SinglePackageResult>;
-export async function inspect(
-  root: string,
-  targetFile: string,
-  options: api.MultiSubprojectInspectOptions,
-): Promise<api.MultiProjectResult>;
-
-// TODO: Specify result types vs any
-// General implementation. The result type depends on the runtime type of `options`.
-export async function inspect(
-  root: string,
-  targetFile: string,
-  options?: Options,
-): Promise<api.InspectResult> {
-  debugLog(
-    "Poetry inspect called with: " +
-    JSON.stringify({
-      root,
-      targetFile,
-      allSubProjects: (options as any)?.allSubProjects,
-      subProject: (options as any)?.subProject,
-    }),
-  );
-
-  const depGraph = new DepGraphBuilder({ name: 'poetry' }).build();
-
-  return {
-    plugin: { name: "plugin" },
-    package: { },
-    dependencyGraph: depGraph
-  };
-}
-
 
 export function buildDepGraph(
   manifestFileContents: string,
   lockFileContents: string,
-  includeDev = false,
 ): DepGraph {
   if (!lockFileContents?.trim()) {
     throw new Error('lockFileContents is missing');
@@ -77,21 +13,36 @@ export function buildDepGraph(
 
   const manifestFile: PoetryManifestType = toml.parse(manifestFileContents);
   const lockFile: PoetryLockFile = toml.parse(lockFileContents);
-  console.log('manifestFile: ', manifestFile);
-  console.log('lockFile: ', lockFile);
 
   const pkgManager: PkgManager = { name: 'poetry' };
+
   const builder = new DepGraphBuilder(pkgManager);
 
   const poetryDependencies = manifestFile.tool.poetry.dependencies;
-  Object.keys(poetryDependencies).forEach((packageName: string) => {
-    const pkg = lockFile.package.find((lockItem) => {
-      return lockItem.name == packageName;
+
+  Object.keys(poetryDependencies)
+    .filter(pkgName => pkgName != 'python')
+    .forEach(pkgName => {
+      addDependenciesFor(pkgName, builder.rootNodeId);
     });
-    const pkgInfo = { name: packageName, version: pkg!.version }
-    builder.addPkgNode(pkgInfo, packageName);
-    builder.connectDep(builder.rootNodeId, packageName);
-  });
 
   return builder.build();
+
+  function addDependenciesFor(packageName: string, parentNodeId: string) {
+    const pkg = lockFile.package.find((lockItem) => {
+      return lockItem.name.toLowerCase() === packageName.toLowerCase();
+    });
+    const pkgInfo = { name: packageName, version: pkg!.version };
+    builder
+      .addPkgNode(pkgInfo, packageName)
+      .connectDep(parentNodeId, packageName);
+
+    const childDependencies = pkg!.dependencies
+    if (childDependencies) {
+      Object.keys(childDependencies)
+        .forEach((subPkgName: string) => {
+          addDependenciesFor(subPkgName, packageName);
+        })
+    }
+  }
 }
--- test/fixtures/index.test.ts
+++ test/fixtures/index.test.ts
@@ -4,26 +4,40 @@ import { DepGraphBuilder } from '@snyk/dep-graph';
 
 
 describe('buildDepGraph', () => {
-
   let depGraphBuilder: DepGraphBuilder;
 
   beforeEach(() => {
     depGraphBuilder = new DepGraphBuilder({ name: "poetry" });
   });
 
+  it('oneDepWithTransitive yields graph with the two packages', async() => {
+    const expectedGraph = depGraphBuilder
+      .addPkgNode({ name: "jinja2", version: "2.11.2" }, "jinja2")
+      .connectDep(depGraphBuilder.rootNodeId, "jinja2")
+      .addPkgNode({ name: "MarkupSafe", version: "1.1.1" }, "MarkupSafe")
+      .connectDep("jinja2", "MarkupSafe").build();
+
+    expect((depGraphForScenarioAt('scenarios/one-dep-with-transitive'))
+      .equals(expectedGraph)).toBe(true);
+  })
+
   it('oneDepNoTransitives yields a graph with only package and its dep', async () => {
-    const { manifestFileContents, lockFileContents } = readFixture('scenarios/one-dep-no-transitives');
-    const actualGraph = await buildDepGraph(
-      manifestFileContents,
-      lockFileContents,
-    );
+    const expectedGraph = depGraphBuilder
+      .addPkgNode({ name: "six", version: "1.15.0" }, "six")
+      .connectDep(depGraphBuilder.rootNodeId, "six").build();
 
-    depGraphBuilder.addPkgNode({ name: "six", version: "1.15.0" }, "six");
-    depGraphBuilder.connectDep(depGraphBuilder.rootNodeId, "six");
-    const expectedGraph = depGraphBuilder.build();
+    expect(depGraphForScenarioAt('scenarios/one-dep-no-transitives')
+      .equals(expectedGraph)).toBe(true);
 
-    const areGraphsEqual = expectedGraph.equals(actualGraph);
-    expect(areGraphsEqual).toBe(true);
   });
 
 })
+
+function depGraphForScenarioAt(scenarioPath: string) {
+  const { manifestFileContents, lockFileContents } = readFixture(scenarioPath);
+  return buildDepGraph(
+    manifestFileContents,
+    lockFileContents,
+  );
+}
+
