ea344c1a819b77c42cc61edd31ff0a330d75b215
Daniel Trunley <daniel.trunley@snyk.io>
2021-02-07 20:20:27 +0000

    bug: ignore packages in virtualenv

--- lib/poetry-dep-graph-builder.ts
+++ lib/poetry-dep-graph-builder.ts
@@ -1,6 +1,16 @@
 import { DepGraph, DepGraphBuilder, PkgInfo } from '@snyk/dep-graph';
 import { PoetryLockFileDependency } from './lock-file-parser';
 
+// Poetry uses the virtualenv to create an environment and this comes with these
+// packages pre-installed, therefore they won't be part of the lockfile.
+// See: https://github.com/python-poetry/poetry/issues/3075#issuecomment-703334427
+const IGNORED_DEPENDENCIES: string[] = [
+  'setuptools',
+  'distribute',
+  'pip',
+  'wheel',
+];
+
 export function build(
   pkgDetails: PkgInfo,
   pkgDependencyNames: string[],
@@ -16,7 +26,7 @@ export function build(
   return builder.build();
 }
 
-export function addDependenciesToGraph(
+function addDependenciesToGraph(
   pkgNames: string[],
   pkgSpecs: PoetryLockFileDependency[],
   parentNodeId: string,
@@ -27,18 +37,20 @@ export function addDependenciesToGraph(
   }
 }
 
-export function addDependenciesForPkg(
+function addDependenciesForPkg(
   pkgName: string,
   pkgSpecs: PoetryLockFileDependency[],
   parentNodeId: string,
   builder: DepGraphBuilder,
 ) {
+  if (IGNORED_DEPENDENCIES.includes(pkgName)) {
+    return;
+  }
   // Poetry will auto-resolve dependencies with hyphens to dashes, but keep transitive reference name with underscore
   pkgName = pkgName.replace(/_/g, '-');
 
   const pkg = pkgLockInfoFor(pkgName, pkgSpecs);
   if (isPkgAlreadyInGraph(pkg, builder)) {
-    builder.connectDep(parentNodeId, pkgName);
     return;
   }
 
@@ -47,28 +59,27 @@ export function addDependenciesForPkg(
   addDependenciesToGraph(pkg.dependencies, pkgSpecs, pkgName, builder);
 }
 
-export function isPkgAlreadyInGraph(
+function isPkgAlreadyInGraph(
   pkg: PoetryLockFileDependency,
   builder: DepGraphBuilder,
 ): boolean {
-  const existingPkg = builder
+  return builder
     .getPkgs()
-    .find(
+    .some(
       (existingPkg) =>
         existingPkg.name === pkg.name && existingPkg.version === pkg.version,
     );
-  return !!existingPkg;
 }
 
-export function pkgLockInfoFor(
+function pkgLockInfoFor(
   pkgName: string,
   pkgSpecs: PoetryLockFileDependency[],
 ): PoetryLockFileDependency {
-  const pkgLockInfo = pkgSpecs.find((lockItem) => {
-    return lockItem.name.toLowerCase() === pkgName.toLowerCase();
-  });
+  const pkgLockInfo = pkgSpecs.find(
+    (lockItem) => lockItem.name.toLowerCase() === pkgName.toLowerCase(),
+  );
 
-  if (pkgLockInfo === undefined) {
+  if (!pkgLockInfo) {
     throw new DependencyNotFound(pkgName);
   }
   return pkgLockInfo;
@@ -77,6 +88,6 @@ export function pkgLockInfoFor(
 export class DependencyNotFound extends Error {
   constructor(pkgName: string) {
     super(`Unable to find dependencies in poetry.lock for package: ${pkgName}`);
-    this.name = 'DependencyNotFound';
+    this.name = DependencyNotFound.name;
   }
 }
--- test/unit/lib/poetry-dep-graph-builder.test.ts
+++ test/unit/lib/poetry-dep-graph-builder.test.ts
@@ -1,158 +1,93 @@
 import {
-  addDependenciesForPkg,
+  build,
   DependencyNotFound,
-  isPkgAlreadyInGraph,
-  pkgLockInfoFor,
 } from '../../../lib/poetry-dep-graph-builder';
 import { PoetryLockFileDependency } from '../../../lib/lock-file-parser';
-import { DepGraphBuilder, PkgInfo, PkgManager } from '@snyk/dep-graph';
+import { PkgInfo } from '@snyk/dep-graph';
 
 describe('poetry-dep-graph-builder', () => {
-  const pkgA: PoetryLockFileDependency = {
-    name: 'pkg-a',
-    version: '1.0',
-    dependencies: [],
-  };
-  const pkgB: PoetryLockFileDependency = {
-    name: 'pkg-b',
-    version: '2.0',
-    dependencies: [],
-  };
-  let pkgSpecs: PoetryLockFileDependency[] = [pkgA, pkgB];
+  const rootPkg: PkgInfo = { name: 'RootPkg', version: '1.0.0' };
 
-  describe('addDependenciesForPkg', () => {
-    let builder: DepGraphBuilder;
-    let rootPkg: PkgInfo;
-    beforeEach(() => {
-      rootPkg = { name: 'TestPkg', version: '1.0' };
-      const pkgManager: PkgManager = { name: 'Poetry' };
-      builder = new DepGraphBuilder(pkgManager, rootPkg);
-    });
-
-    it('should addPkgNode and connectDep if pkg is new to graph', () => {
+  describe('build', () => {
+    it('should return a graph of given dependencies successfully', () => {
       // given
-      builder.addPkgNode = jest.fn().mockReturnThis();
-      builder.connectDep = jest.fn();
+      const pkgA = generatePoetryLockFileDependency('pkg-a');
+      const pkgB = generatePoetryLockFileDependency('pkg-b', ['pkg-c']);
+      const pkgC = generatePoetryLockFileDependency('pkg-c');
+      const pkgSpecs: PoetryLockFileDependency[] = [pkgA, pkgB, pkgC];
 
       // when
-      addDependenciesForPkg(pkgA.name, pkgSpecs, rootPkg.name, builder);
+      const result = build(rootPkg, ['pkg-a', 'pkg-b'], pkgSpecs);
 
       // then
-      expect(builder.connectDep).toHaveBeenCalledWith(rootPkg.name, pkgA.name);
-      const expectedPkgInfo: PkgInfo = {
-        name: pkgA.name,
-        version: pkgA.version,
-      };
-      expect(builder.addPkgNode).toHaveBeenCalledWith(
-        expectedPkgInfo,
-        pkgA.name,
+      const resultGraph = result.toJSON().graph;
+      expect(resultGraph.nodes).toHaveLength(4);
+      const rootNode = resultGraph.nodes.find(
+        (node) => node.pkgId === `${rootPkg.name}@${rootPkg.version}`,
       );
+      expect(rootNode!.deps).toHaveLength(2);
+      const nodeWithTransitive = resultGraph.nodes.find(
+        (node) => node.nodeId === pkgB.name,
+      );
+      expect(nodeWithTransitive!.deps).toHaveLength(1);
+      expect(nodeWithTransitive!.deps[0].nodeId).toBe(pkgC.name);
     });
 
-    it('should only connect nodes if pkg already exists', () => {
+    it('should ignore poetry installed virtualenv packages as transitives', () => {
       // given
-      builder.addPkgNode(pkgA, rootPkg.name);
-      builder.connectDep = jest.fn();
-      builder.addPkgNode = jest.fn();
+      const pkgA = generatePoetryLockFileDependency('pkg-a');
+      const wheel = generatePoetryLockFileDependency('wheel');
+      const setuptools = generatePoetryLockFileDependency('setuptools');
+      const distribute = generatePoetryLockFileDependency('distribute');
+      const pip = generatePoetryLockFileDependency('pip');
+      pkgA.dependencies = [
+        wheel.name,
+        setuptools.name,
+        distribute.name,
+        pip.name,
+      ];
 
       // when
-      addDependenciesForPkg(pkgA.name, pkgSpecs, rootPkg.name, builder);
+      const result = build(rootPkg, [pkgA.name], [pkgA]);
 
       // then
-      expect(builder.connectDep).toHaveBeenCalledWith(rootPkg.name, pkgA.name);
-      expect(builder.addPkgNode).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('isPkgAlreadyInGraph', () => {
-    let builder: DepGraphBuilder;
-    beforeEach(() => {
-      const rootPkg: PkgInfo = { name: 'TestPkg', version: '1.0' };
-      const pkgManager: PkgManager = { name: 'Poetry' };
-      builder = new DepGraphBuilder(pkgManager, rootPkg);
-      builder.addPkgNode(pkgA, pkgA.name);
-      builder.addPkgNode(pkgB, pkgB.name);
+      const resultGraph = result.toJSON().graph;
+      expect(resultGraph.nodes).toHaveLength(2);
+      const nodeWithVirtualEnvDeps = resultGraph.nodes.find(
+        (node) => node.nodeId === pkgA.name,
+      );
+      expect(nodeWithVirtualEnvDeps!.deps).toHaveLength(0);
     });
 
-    it('should return true if pkg has already been added to builder', () => {
-      // given / when
-      const result = isPkgAlreadyInGraph(pkgA, builder);
-
-      // then
-      expect(result).toBeTruthy();
-    });
-
-    it('should return false if pkg has not already been added to builder', () => {
+    it('should not add node twice when there are circular dependencies', () => {
       // given
-      const pkgC: PoetryLockFileDependency = {
-        name: 'pkg-c',
-        version: '3.0',
-        dependencies: [],
-      };
+      const pkgA = generatePoetryLockFileDependency('pkg-a', ['pkg-b']);
+      const pkgB = generatePoetryLockFileDependency('pkg-b', ['pkg-a']);
 
       // when
-      const result = isPkgAlreadyInGraph(pkgC, builder);
+      const result = build(rootPkg, [pkgA.name, pkgB.name], [pkgA, pkgB]);
 
       // then
-      expect(result).toBeFalsy();
+      expect(result).toBeDefined();
+      const aNodes = result
+        .toJSON()
+        .graph.nodes.filter((node) => node.nodeId === 'pkg-a');
+      const bNodes = result
+        .toJSON()
+        .graph.nodes.filter((node) => node.nodeId === 'pkg-b');
+      expect(aNodes).toHaveLength(1);
+      expect(bNodes).toHaveLength(1);
     });
 
-    it('should return false if pkgName exists but no matching version', () => {
+    it('should throw DependencyNotFound if metadata cannot be found in pkgSpecs', () => {
       // given
-      const alternatePkgB: PoetryLockFileDependency = {
-        name: pkgB.name,
-        version: '1.0',
-        dependencies: [],
-      };
-
-      // when
-      const result = isPkgAlreadyInGraph(alternatePkgB, builder);
-
-      // then
-      expect(result).toBeFalsy();
-    });
-  });
-
-  describe('pkgLockInfoFor', () => {
-    let pkgSpecs: PoetryLockFileDependency[] = [];
-    const pkgC: PoetryLockFileDependency = {
-      name: 'pkg-C',
-      version: '3.0',
-      dependencies: [],
-    };
-    beforeEach(() => {
-      pkgSpecs = [pkgA, pkgB, pkgC];
-    });
-
-    it('should retrieve correct item from pkgSpecs when pkgName exists', () => {
-      // given
-      const pkgName = 'pkg-b';
-
-      // when
-      const result = pkgLockInfoFor(pkgName, pkgSpecs);
-
-      // then
-      expect(result).toEqual(pkgB);
-    });
-
-    it('should retrieve correct item ignoring case sensitivity', () => {
-      // given
-      const pkgName = 'PKG-c';
-
-      // when
-      const result = pkgLockInfoFor(pkgName, pkgSpecs);
-
-      // then
-      expect(result).toEqual(pkgC);
-    });
-
-    it('should throw DependencyNotFound if pkgName does not exist in pkgSpecs', () => {
-      // given
-      const pkgName = 'pkg-d';
+      const pkgA = generatePoetryLockFileDependency('pkg-a', [
+        'non-existent-pkg',
+      ]);
 
       // when
       const errorResult = () => {
-        pkgLockInfoFor(pkgName, pkgSpecs);
+        build(rootPkg, [pkgA.name], [pkgA]);
       };
 
       // then
@@ -160,3 +95,10 @@ describe('poetry-dep-graph-builder', () => {
     });
   });
 });
+
+function generatePoetryLockFileDependency(
+  pkgName: string,
+  dependencies: string[] = [],
+): PoetryLockFileDependency {
+  return { name: pkgName, version: '1.0.0', dependencies: dependencies };
+}
